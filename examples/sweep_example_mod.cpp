d5f962393e96e4a47eeb38d2f0bbea00bb381eff
Alexandru Cristea <alexcristeap@yahoo.com>
2026-08-20 12:29:14 +0300

    Modify ports for knee and hip joint control
    Updated port configuration for knee and hip joints.

--- examples/sweep_example_mod.cpp
+++ examples/sweep_example_mod.cpp
@@ -6,9 +6,7 @@ using namespace std;
 int main() {
     Pci2321 dev;
     pair<Port, int> ports[] = {
-        {Port::P1C, 18}, {Port::P1C, 19}, {Port::P2A, 28},
-        {Port::P2A, 29}, {Port::P2A, 30},
-        {Port::P2A, 31}
+        {Port::P1C, 20}, {Port::P1C, 21}, {Port::P1C, 22}, {Port::P1C, 23}, {Port::P2A, 29} // releases the brakes for the knee (bits 20, 21) and hip (bits 22, 23) joints
     };
 
     for (auto [pt, bit] : ports) {
